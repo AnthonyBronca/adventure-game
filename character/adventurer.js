dcddbb6b1720a83221646732a26662febe69c026
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-03-23 03:10:03 -0400

    added PST

--- character/adventurer.js
+++ character/adventurer.js
@@ -1,8 +1,48 @@
 const defaultGear = require('../gear/default_gear')
+const random = require('getrandomjs')
+
+
 
 
 class Adventurer {
-    constructor() {
+    constructor(name, level = 3, health = 10, gear = defaultGear) {
+        this.name = name;
+        this.level = level;
+        this.health = health;
+        this.gear = gear;
     }
 
+    //instance methods
+    changeName(newName) {
+        this.name = newName;
+    }
+
+    //level up function that levels up one instance of a char
+
+    powerUp() {
+        this.level++;
+        let newHealth = random(1, 5)
+        this.health += newHealth;
+    }
+
+
 }
+
+
+// const ryan = new Adventurer('ryan')
+// const anthony = new Adventurer('anthony')
+
+module.exports = Adventurer
+
+
+
+
+// console.log(ryan)
+
+// ryan.changeName('Ryan');
+
+// console.log(ryan)
+// // console.log(anthony)
+
+// ryan.powerUp()
+// console.log(ryan)
