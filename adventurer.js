02c2c747d8eba529c87e0825e45b9db1123e39f6
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-03-22 20:17:51 -0400

    added files

---
+++ adventurer.js
@@ -0,0 +1,15 @@
+const defaultGear = {
+    head: null,
+    torso: 'shirt',
+    legs: 'pants',
+    mainHand: null,
+    offHand: null,
+    twohanded: false
+}
+
+class Adventurer{
+constructor(){
+
+}
+
+}
