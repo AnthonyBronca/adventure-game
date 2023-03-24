3026b3eb2dc43d0d2619753a1baf319d959663c4
Anthony Bronca <95654116+AnthonyBronca@users.noreply.github.com>
2023-03-24 03:14:23 -0400

    added the ability to add and remove buffs, spells, debuffs, and familiars

--- character/wizard.js
+++ character/wizard.js
@@ -18,7 +18,7 @@ class Wizard {
             let name = spell.name
             this.spells[name] = spell;
         } else {
-            Error('user already knows that spell');
+            throw new Error('user already knows that spell');
         }
     }
     addDebuff(debuff) {
@@ -26,22 +26,76 @@ class Wizard {
             let name = debuff.name
             this.debuffs[name] = debuff;
         } else {
-            Error('user already has that debuff');
+            throw new Error('user already has that debuff');
         }
     }
     //todo
-    addBuff() {
-
+    addBuff(buff) {
+        if (!this.buffs[buff.name]) {
+            let name = buff.name
+            this.buffs[name] = buff;
+        } else {
+            throw new Error('user already has that buff');
+        }
     }
     //todo
-    addFamiliar() {
+    addFamiliar(familiar) {
+        if (this.familiar === null) {
+            this.familiar = familiar;
+        } else {
+            throw new Error('user already has a familiar');
+        }
 
     }
-    //todo, add remove for all the methods above ^
+
+    removeSpell(spell) {
+        let name = spell.name;
+        if (this.spells[name]) {
+            delete this.spells[name];
+        } else {
+            throw new Error('User does not know that spell')
+        }
+    }
+
+    removeDebuff(debuff) {
+        let name = debuff.name;
+        if (this.debuffs[name]) {
+            delete this.debuffs[name];
+        } else {
+            throw new Error('User does not have that debuff')
+        }
+    }
+
+    removeBuff(buff) {
+        let name = buff.name;
+        if (this.buffs[name]) {
+            delete this.buffs[name];
+        } else {
+            throw new Error('User does not have that buff')
+        }
+
+    }
+
+    removeFamiliar() {
+        if (this.familiar !== null) {
+            this.familiar = null;
+        } else {
+            throw new Error('User does not have that familiar')
+        }
+    }
 
 }
 
 let test = new Wizard()
-test.addSpell()
+
+let fire = { name: 'fire', description: 'shoots fire' }
+
+test.addFamiliar(fire)
+console.log(test)
+test.removeFamiliar(fire)
+
+console.log(test)
+
+
 
 module.exports = Wizard;
